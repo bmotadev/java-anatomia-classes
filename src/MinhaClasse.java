fcfece1adf7e77d85d17439e11f56589a3f8a60e
Bruno Mota <bmotadev@gmail.com>
2023-02-10 14:02:48 -0300

    Anatomia de classes - Parte 3 - Metodos - Concatenação - Tipos de variáveis

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,5 +1,21 @@
 public class MinhaClasse {
     public static void main(String [] args) {
-        System.out.print("Hello World!");
+        // String meuNome = "Bruno";
+
+        // int anoFabricacao = 1700;
+
+        // boolean verdadeira = true; // true or false
+
+        // anoFabricacao = 2018;
+
+        String primeiroNome = "Bruno";
+        String segundoNome = "Mota";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
